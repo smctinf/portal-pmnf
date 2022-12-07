efe1a89bff13412ed961198fd299580116130cb4
Eduardo Salarini <luiseduardo@id.uff.br>
2022-12-07 15:52:59 -0300

    teste de layout

--- core/views.py
+++ core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context={
+        'titulo': 'PORTAL DE SERVIÇOS ONLINE'
+    }
+    return render(request, 'index.html', context)
